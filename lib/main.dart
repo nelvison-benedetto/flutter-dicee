1f97090ccdeeaa3dc1251cc6e4dff8430b33b194
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-10-16 13:45:08 +0200

    project done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text(
+              'Dicee',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+          ),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +23,58 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftNum = 1;
+  int rightNum = 1;
+  Map<String,int> numsR = {};
+
+  void randomFaces() {
+    setState(() {  //setState update the stare recall the build
+      leftNum = Random().nextInt(6) + 1;
+      rightNum = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            //flex:1,
+              child: TextButton(
+                  onPressed:(){
+                    randomFaces();
+                  },
+                  child: Image.asset('images/dice$leftNum.png')
+              )
+          ),
+          //Expanded must be DIRECT child of Column/Row/Flex!!otherwise error
+          Expanded(
+            //flex:1,
+              child: TextButton(
+                onPressed:(){
+                  randomFaces();
+                },
+                child: Image(
+                  image: AssetImage('images/dice$rightNum.png'),  //Short method
+                ),
+              )
+          ),
+        ],
+      ),
+    );
   }
+}
+
+Map<String,int> getRandomsNums(leftNum,rightNum){
+  int leftNum= Random().nextInt(6)+1;
+  int rightNum= Random().nextInt(6)+1;
+  return {'leftNum':leftNum, 'rightNum':rightNum};
 }
